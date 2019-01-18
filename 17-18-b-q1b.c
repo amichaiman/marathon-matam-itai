63e8dc2b6e9bb9a9d2cbb619338199f9871871e3
amichaiman <ami.dan.man@gmail.com>
2019-01-18 17:54:58 +0200

    first commit

---
+++ 17-18-b-q1b.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void* max(void* a, void* b) {
+    return (long)a > (long)b ? a : b;
+}
+
+void* find_max(void** array, int size, void*(*max)(void*, void*)) {
+    void* biggest=array[0];
+    int i;
+    for (i=1; i<size; i++) {
+        if (max(biggest, array[i]) == array[i]) {
+            biggest = array[i];
+        }
+    }
+    return biggest;
+}
+
+int main() {
+    long array[] = {1,4,7,3,9,3,3,8,6};
+
+    printf("The biggest element is: %d\n", (int)find_max((void **) array, sizeof(array) / sizeof(*array), max));
+
+    return 0;
+}
